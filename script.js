aec30d23f8928c3d9d241584db83e459a21ff76d
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-08-08 20:19:37 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(window).scroll(function () {
-        if (this.scrollY > 20){
-            $('.navbar').addClass('sticky');
-        } else{
-            $('.navbar').removeClass('sticky');
-        }      
-    });
-});
